a8114525788b5d7433fedf3a0cfef41cd7e73605
hrushigawade24 <hrushigawade24@gmail.com>
2024-09-10 00:03:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="product">
+        <h1>Stock Market Average Calculator</h1>
+        <h3>First Purchase</h3>
+        <input className="inputBox" type="text" placeholder="Enter Unit's Size" />
+        <input className="inputBox" type="text" placeholder="Enter Price of Share" />
+        <h3>Second Purchase</h3>
+        <input className="inputBox" type="text" placeholder="Enter Unit's Size" />
+        <input className="inputBox" type="text" placeholder="Enter Price of Share" />
+        <button className='appButton'>Clear Fields</button>
+        <button className='appButton'>Calculate Average</button>
+        <div className='footer'><h6> Powered By @Hybrid Engineer</h6></div>
+      </div>
     </div>
   );
 }
